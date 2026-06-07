7c9451cbd4cb37f21486e0c8a58af17fe24ead45
PC_user <PC_user@DESKTOP-M0EO74D>
2026-06-07 12:43:19 +0900

    Spring Securityによる認証機能の実装

---
+++ src/main/java/com/lesson/memo/model/Admin.java
@@ -0,0 +1,47 @@
+package com.lesson.memo.model;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+@Entity
+@Data
+public class Admin  {
+	 
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+	@NotNull(message = "姓を入力してください")
+	@NotBlank(message = "姓を入力してください")
+    private String last_name;
+
+	@NotNull(message = "名を入力してください")
+	@NotBlank(message = "名を入力してください")
+    private String first_name;
+    
+	@NotBlank(message = "メールアドレスを入力してください")
+	@Email(message = "メールアドレス形式で入力してください")
+    private String email;
+    
+	@NotBlank(message = "パスワードを入力してください")
+	@Column(nullable = false)
+	private String password;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+	 
+	 
+}
